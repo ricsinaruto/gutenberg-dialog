b4ac0cf312f07ef634cce6ce0c3099cb3a8660c1
Richard Csaky <ricsinaruto@hotmail.com>
2020-04-05 21:02:37 +0200

    fix setup

--- setup.py
+++ setup.py
@@ -1,7 +1,9 @@
 import os
 
-os.mkdir('data')
-os.mkdir('data/raw')
+if not os.path.exists('data'):
+  os.mkdir('data')
+if not os.path.exists('data/raw'):
+  os.mkdir('data/raw')
 
 print('Installing requirements...')
 os.system('pip install -r requirements.txt')
